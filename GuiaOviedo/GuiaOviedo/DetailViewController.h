56a851f397fc739a730fc5b1dc1935c396884b75
yoye0z7 <yoye0z7@gmail.com>
2012-03-30 10:56:11 +0200

    Primer commit

---
+++ GuiaOviedo/GuiaOviedo/DetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.h
+//  GuiaOviedo
+//
+//  Created by svp on 30.03.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController <UISplitViewControllerDelegate>
+
+@property (strong, nonatomic) id detailItem;
+
+@property (strong, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+
+@end
---
+++ GuiaOviedo/GuiaOviedo/MasterViewController.h
@@ -0,0 +1,22 @@
+//
+//  MasterViewController.h
+//  GuiaOviedo
+//
+//  Created by svp on 30.03.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+#import <CoreData/CoreData.h>
+
+@interface MasterViewController : UITableViewController <NSFetchedResultsControllerDelegate>
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+@property (strong, nonatomic) NSFetchedResultsController *fetchedResultsController;
+@property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+@end
