1c1307905b51437eef79196bfab7a2f0007199c8
yoye0z7 <yoye0z7@gmail.com>
2012-04-03 09:15:24 +0200

    a

--- GuiaOviedo/GuiaOviedo/DetailViewController.h
+++ GuiaOviedo/GuiaOviedo/DetailViewController.h
@@ -7,15 +7,15 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "MapKit/MapKit.h"
 
-# define URLServicio [NSURL URLWithString:@"http://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=39ea99392a509d351c352ab0651080dd&tags=oviedo&sort=date-posted-desc&accuracy=11&content_type=1&has_geo=1&page=5&format=json&nojsoncallback=1&auth_token=72157629328549670-a95610979f24c5f9&api_sig=6802bbfdfae0d192556023f22f1d90f0"]
 
 @interface DetailViewController : UIViewController <UISplitViewControllerDelegate>
 
 @property (strong, nonatomic) id detailItem;
 
 @property (strong, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
-@property(nonatomic,retain)NSMutableArray *listadoFotos;
-@property(nonatomic,retain)NSMutableData *datosInternet;
-- (void)serializar;
+
+@property (strong, nonatomic) IBOutlet MKMapView *mapa;
+
 @end
--- GuiaOviedo/GuiaOviedo/MasterViewController.m
+++ GuiaOviedo/GuiaOviedo/MasterViewController.m
@@ -156,14 +156,14 @@
     // Create the fetch request for the entity.
     NSFetchRequest *fetchRequest = [[NSFetchRequest alloc] init];
     // Edit the entity name as appropriate.
-    NSEntityDescription *entity = [NSEntityDescription entityForName:@"Event" inManagedObjectContext:self.managedObjectContext];
+    NSEntityDescription *entity = [NSEntityDescription entityForName:@"Evento" inManagedObjectContext:self.managedObjectContext];
     [fetchRequest setEntity:entity];
     
     // Set the batch size to a suitable number.
     [fetchRequest setFetchBatchSize:20];
     
     // Edit the sort key as appropriate.
-    NSSortDescriptor *sortDescriptor = [[NSSortDescriptor alloc] initWithKey:@"timeStamp" ascending:NO];
+    NSSortDescriptor *sortDescriptor = [[NSSortDescriptor alloc] initWithKey:@"inicio" ascending:NO];
     NSArray *sortDescriptors = [NSArray arrayWithObjects:sortDescriptor, nil];
     
     [fetchRequest setSortDescriptors:sortDescriptors];
@@ -251,7 +251,7 @@
 - (void)configureCell:(UITableViewCell *)cell atIndexPath:(NSIndexPath *)indexPath
 {
     NSManagedObject *managedObject = [self.fetchedResultsController objectAtIndexPath:indexPath];
-    cell.textLabel.text = [[managedObject valueForKey:@"timeStamp"] description];
+    cell.textLabel.text = [[managedObject valueForKey:@"inicio"] description];
 }
 
 - (void)insertNewObject
@@ -263,7 +263,7 @@
     
     // If appropriate, configure the new managed object.
     // Normally you should use accessor methods, but using KVC here avoids the need to add a custom class to the template.
-    [newManagedObject setValue:[NSDate date] forKey:@"timeStamp"];
+    [newManagedObject setValue:[NSDate date] forKey:@"inicio"];
     
     // Save the context.
     NSError *error = nil;
